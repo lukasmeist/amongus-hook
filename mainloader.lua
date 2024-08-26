2ba8047326fe34a8bffb846fc82ebfd6d13ec6eb
lukasmeist <lukasmeist@gmail.com>
2024-08-26 05:41:20 +0300

    Add files via upload

---
+++ mainloader.lua
@@ -0,0 +1,46 @@
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end;
+
+local messagebox = messagebox or function(message, title, id) warn(`[{title} - {message}`);end;
+local request = request or http_request;
+local loadstring = loadstring;
+
+if (not request) then
+	return messagebox('request function is unsupported!');
+elseif (not loadstring) then
+	return messagebox('loadstring function is unsupported!');
+elseif (not Drawing) then
+	return messagebox('Drawing function was not found! (Executor issue)')
+end;
+
+local placeid = game.PlaceId;
+local dir = 'https://raw.githubusercontent.com/mainstreamed/amongus-hook/main/';
+
+local statuslist = {
+	['fallensurvival'] = {
+		name = 'Fallen Survival',
+		status = 'Detected',
+	},
+	['tridentsurvival'] = {
+		name = 'Trident Survival',
+		status = 'Undetected',
+	},
+};
+
+local load = function(name)
+	local game = statuslist[name];
+	if (game.status ~= 'Undetected') then
+		if (messagebox(`{game.name} is Currently Marked as {game.status}!\n\nAre You Sure You Want to Continue?`, `amongus.hook`, 52) ~= 6) then
+			return;
+		end;
+	end;
+	loadstring(request({Url=`{dir}{name}/main.lua`,Method='GET'}).Body)();
+end;
+
+if (placeid == 13253735473) then
+	return load('tridentsurvival');
+elseif (placeid == 13800717766 or placeid == 15479377118) then
+	return load('fallensurvival');
+end;
+messagebox(`This Game is Unsupported!\n\nIf you believe this is incorrect, please open a ticket in our discord! - discord.gg/2jycAcKvdw`, `amongus.hook [{placeid}]`, 48);
